c23016af62d770c64628675e51555016336d118a
paulooliveira152012 <paulooliveira152012@gmail.com>
2024-11-30 19:20:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Welcome to My PWA</h1>
+      <p>This is a simple Progressive Web App.</p>
+      <button onClick={() => alert('Hello!')}>Click Me</button>
     </div>
   );
 }
