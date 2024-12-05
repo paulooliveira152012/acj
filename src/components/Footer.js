f3ded9c2841c8c856c8c711a0d9d2daaf9e95cac
paulooliveira152012 <paulooliveira152012@gmail.com>
2024-12-04 21:50:20 -0500

    done footer first version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,43 @@
-import '../styles/style.css'
-import { Link } from 'react-router-dom'
+import "../styles/style.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div>
-                <h2>business Info</h2>
-            </div>
-            <div>
-                <h2>links</h2>
-                <ul>
-                    <li>
-                    <Link to="/accessibility-statement">Accessibility Statement</Link> 
-                    </li>
-                    <li>
-                    <Link to="/privacy-policy" >Privacy Policy</Link>
-                    </li>
-                    
-                </ul>
-            </div>
-            <div>
-                <h2>adm</h2>
-                <ul>
-                    <li>
-                        <Link to='/admLogin'>
-                            login
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className="footer">
+      {/* flex side by side */}
+      {/* section1 */}
+      <div className="section flex">
+        <div className="logoContainer">Logo</div>
+        <div className="addressContainer">
+          <span>Address:</span> 570 Maple Ave, Elizabeth, NJ 07202 <br />
+          <span>Telephone:</span> +1 908-527-9734
+        </div>
+      </div>
+
+      {/* section 2 */}
+      <div className="section links">
+        <Link to="/about-us">About Us</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
+      {/* section 3 */}
+      <div className="section links">
+        <Link to="/privacy-policy">Privacy Policy</Link>
+        <Link to="/accessibility-statement">Accessibility Statement</Link>
+        <Link to="/admLogin">login (Adm)</Link>
+      </div>
+
+      {/* section 4 */}
+      <div className="section links">
+        <p>
+          &copy; {currentYear} ACJ Auto Repair. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
