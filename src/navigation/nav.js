9c93432aff2c0a027b8ae63114ee88eb1f26e6bf
paulooliveira152012 <paulooliveira152012@gmail.com>
2024-12-14 00:08:24 -0500

    working on marking current page

--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -1,20 +1,75 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import "../styles/style.css";
 
 const NavBar = ({ className, handleTabClick }) => {
-    return (
-        <nav className={className}>
-        <nav className={className}>
-            <Link to="/" onClick={handleTabClick}>Home</Link> 
-            <Link to="/about" onClick={handleTabClick}>About Us</Link> 
-            <Link to="/our-services" onClick={handleTabClick}>Our Services</Link>
-            <Link to="/tips-and-advice" onClick={handleTabClick}>Tips & Advice</Link> 
-            <Link to="/contact-us" onClick={handleTabClick}>Contact</Link> 
-            {/* <Link to="/accessibility-statement" onClick={handleTabClick}>Accessibility Statement</Link>  */}
-            {/* <Link to="/privacy-policy" onClick={handleTabClick}>Privacy Policy</Link> */}
-        </nav>
-    </nav>
-    )
-}
+  const location = useLocation();
+  const [currentScreen, setCurrentScreen] = useState("");
 
-export default NavBar
+  console.log(location.pathname);
+
+  // Update currentScreen based on location.pathname
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setCurrentScreen("home");
+    } else if (location.pathname === "/about") {
+      setCurrentScreen("about");
+    } else if (location.pathname === "/our-services") {
+      setCurrentScreen("our-services");
+    } else if (location.pathname === "/tips-and-advice") {
+      setCurrentScreen("tips-and-advice");
+    } else if (location.pathname === "/contact-us") {
+      setCurrentScreen("contact");
+    }
+  }, [location]);
+
+  console.log("Current path:", location.pathname);
+  console.log("Current screen is:", currentScreen);
+
+  return (
+    <nav className={className}>
+      <nav className={className}>
+        <Link
+          to="/"
+          onClick={handleTabClick}
+          className={currentScreen === "home" ? "currentScreen" : ""}
+        >
+          Home
+        </Link>
+        <Link 
+            to="/about" 
+            onClick={handleTabClick}
+            className={currentScreen === "about" ? "currentScreenBlack" : ""}
+        >
+          About Us
+        </Link>
+        <Link 
+            to="/our-services" 
+            onClick={handleTabClick}
+            className={currentScreen === "our-services" ? "currentScreenBlack" : ""}
+        >
+          Our Services
+        </Link>
+        <Link 
+            to="/tips-and-advice" 
+            onClick={handleTabClick}
+            className={currentScreen === "tips-and-advice" ? "currentScreenBlack" : ""}
+        >
+          Tips & Advice
+        </Link>
+        <Link 
+            to="/contact-us" 
+            onClick={handleTabClick}
+            className={currentScreen === "contact" ? "currentScreenBlack" : ""}
+            >
+          Contact
+        </Link>
+        {/* <Link to="/accessibility-statement" onClick={handleTabClick}>Accessibility Statement</Link>  */}
+        {/* <Link to="/privacy-policy" onClick={handleTabClick}>Privacy Policy</Link> */}
+      </nav>
+    </nav>
+  );
+};
+
+export default NavBar;
